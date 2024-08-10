a628358ab3e3f75e985a2d499153164c63d344bd
Sam <sam.q.ldn2022@proton.me>
2024-08-10 04:02:33 -0700

    database model changes

--- app/models/Userdb.py
+++ app/models/Userdb.py
@@ -1,14 +1,14 @@
 from sqlalchemy import Boolean, Column, ForeignKey, Integer, String, Enum as dbEnum
 from sqlalchemy.orm import relationship
-from ..schemas.user import RoleType
-from ..database import Base
+from app.schemas.user import RoleType
+from app.database import Base
 
 class User(Base):
     __tablename__ = "users"
 
     id = Column(Integer, primary_key=True)
     email = Column(String, unique=True, index=True)
-    hashed_password = Column(String)
-    role = Column(dbEnum(RoleType), default=True)
+    password = Column(String)
+    isAdmin = Column(bool, default=True)
 
     issues = relationship("Issue", back_populates="user")
