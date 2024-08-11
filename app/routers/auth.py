0b460f84b51f4276d5cb8d8a55345132f36ad4a6
Sam <sam.q.ldn2022@proton.me>
2024-08-11 08:58:56 -0700

    feat: created routes

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -1,10 +1,22 @@
-from fastapi import APIRouter, Request
+from fastapi import APIRouter, Request, Depends
 from fastapi.responses import HTMLResponse
+from services.user import *
+from schemas.user import *
+from database import getDB
 
 router = APIRouter()
 
 
+
 @router.get('/')
-def home(req: Request):
-    context = {'request': req}
-    return  {"message": "auth"}
+async def home(db: Session = Depends(getDB)):
+    return  getUsers(db)
+
+@router.post('/register')
+async def register(user: CreateUser, db: Session = Depends(getDB)):
+    createUser(db,user.email, user.password)
+    return 
+
+@router.delete('/{id}')
+async def delete(id: int, db: Session = Depends(getDB)):
+    deleteUser(db, id)
--- app/routers/issues.py
+++ app/routers/issues.py
@@ -1,16 +1,31 @@
-from fastapi import APIRouter, Request
+from fastapi import APIRouter, Request, Depends
 from fastapi.responses import HTMLResponse
+from schemas.issue import *
+from services.issues import *
+from database import getDB
+
 
 router = APIRouter()
 
+@router.post('/')
+async def postIssue(issue: CreateIssue, db: Session = Depends(getDB) ):
+    return  createIssue(db, issue.title, issue.description, issue.type, issue.user_id)
+
+
+@router.get('/{user_id}')
+async def getIssuesByUser(user_id: int, db: Session = Depends(getDB)):
+    return getIssuesByUser(db, user_id)
 
 @router.get('/')
-def home(req: Request):
-    context = {'request': req}
-    return  {"message": "issues"}
+async def getIssues(db: Session = Depends(getDB)):
+    return  getAllIssues(db)
 
 
-@router.put('/')
-def home(req: Request):
-    context = {'request': req}
+@router.patch('/') 
+def patchIssue(issue: UpdateIssue):
+    context = {'request': issue}
     return  {"message": "issues"}
+
+@router.delete('/{id}')
+async def delete(id: int, db: Session = Depends(getDB)):
+    deleteIssue(db, id)
